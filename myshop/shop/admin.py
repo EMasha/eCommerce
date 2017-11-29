27b674c7fce6805c8a4881fe32c27bc3dad05eb0
GERMAN <enxhimasha91@gmail.com>
2017-11-29 23:21:47 +0100

    german-pc

---
+++ myshop/shop/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'stock', 'available', 'price', 'description', 'image', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated', 'category']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product, ProductAdmin)
